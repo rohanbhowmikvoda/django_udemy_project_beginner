57f890e58bdec0702b60fef0531377e947e686bb
Rohan Bhowmik <rohanbhowmik001@gmail.com>
2026-05-16 19:32:38 +0530

    Delete Functionality

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -48,3 +48,10 @@ def update_item(request,id):
         'form': form
     }
     return render(request,'myapp/item-form.html',context)
+
+def delete_item(request,id):
+    item = Item.objects.get(id=id)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('myapp:index')
+    return render(request,'myapp/item-delete.html')
