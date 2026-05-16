3065d38ed4abfdb5baed034745e564ccbd4098d6
Rohan Bhowmik <rohanbhowmik001@gmail.com>
2026-05-16 20:49:05 +0530

    Register User Functionality

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #INSTALLED_APPS
     'myapp',
+    'users',
 ]
 
 MIDDLEWARE = [
---
+++ mysite/users/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from users import views
+urlpatterns = [
+    path('register/',views.register_user,name='register_user'),
+]
